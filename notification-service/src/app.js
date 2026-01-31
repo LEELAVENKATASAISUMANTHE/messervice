85a6b02bfe59c59a34219dbbf2a8467b9fbc4975
LEELAVENKATASAISUMANTHE <1by23cs111@bmsit.in>
2026-01-31 23:00:46 +0530

    add: integrate token routes into the main application

--- notification-service/src/app.js
+++ notification-service/src/app.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import tokenroutes from './routes/token.routes.js';
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use('/api', tokenroutes);
 // Sample route for sending notifications
 export default app;
