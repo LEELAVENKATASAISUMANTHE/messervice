e6e9508cee7dd1cee999f4a14ac83828c6e7b0c2
LEELAVENKATASAISUMANTHE <1by23cs111@bmsit.in>
2026-01-31 19:53:27 +0530

    chore: initialize project with package.json and express dependency

---
+++ notification-service/src/server.js
@@ -0,0 +1,10 @@
+import app from './app.js';
+
+const PORT = process.env.PORT || 4001;
+app.listen(PORT, () => {
+  console.log(`Notification service is running on port ${PORT}`);
+});
+
+app.get('/', (req, res) => {
+  res.send('Notification Service is up and running!');
+});
