f3c09a1eadde6d0990838976dd913b015034e077
LEELAVENKATASAISUMANTHE <1by23cs111@bmsit.in>
2026-02-03 22:34:39 +0530

    fix: update Kafka brokers configuration to use environment variable

--- notification-service/src/utils/kafka.js
+++ notification-service/src/utils/kafka.js
@@ -2,7 +2,7 @@ import { Kafka } from "kafkajs";
 
 const kafka = new Kafka({
   clientId: "notification-service",
-  brokers: ["172.17.0.1:9092"],
+  brokers: [process.env.KAFKA_BROKERS], // MUST be kafka:9092
 });
 
 export default kafka;
