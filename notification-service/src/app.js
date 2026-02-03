0c34ec3a2e571db502dab1b1f8e2dd84a2e1fd63
LEELAVENKATASAISUMANTHE <1by23cs111@bmsit.in>
2026-02-03 19:00:12 +0530

    fix: remove unused token routes and ensure web notification routes are correctly integrated

--- notification-service/src/app.js
+++ notification-service/src/app.js
@@ -1,12 +1,11 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import tokenroutes from './routes/token.routes.js';
-import webnotificationroutes from './routes/webnotification.routes.js';
+
 const app = express();
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/', tokenroutes);
+
 app.use('/', webnotificationroutes);
 
 // Sample route for sending notifications
--- notification-service/src/server.js
+++ notification-service/src/server.js
@@ -1,9 +1,10 @@
 import app from "./app.js";
 import tokenRoutes from "./routes/token.routes.js";
-
+import webnotificationroutes from './routes/webnotification.routes.js';
 
 // 🔑 THIS LINE IS REQUIRED
 app.use("/notify", tokenRoutes);
+app.use('/notify', webnotificationroutes);
 
 // optional health/root routes
 app.get("/notify", (req, res) => {
