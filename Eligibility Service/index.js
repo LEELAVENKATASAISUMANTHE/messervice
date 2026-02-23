0590d82f9da41fcb7a0a165f6fd8f846afce9a89
LEELAVENKATASAISUMANTHE <1by23cs111@bmsit.in>
2026-02-23 22:27:08 +0530

    fix: correct import paths in index.js and main.js for proper module resolution

--- Eligibility Service/index.js
+++ Eligibility Service/index.js
@@ -2,7 +2,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import { getmessage } from './utils/main.js';
-import * as db from './db/db.js';
+// import * as db from './db/db.js';
 
 
 const app = express();
