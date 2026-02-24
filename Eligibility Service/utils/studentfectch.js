18231fd4ce7533c5f70fda757b45d8695d4152ed
LEELAVENKATASAISUMANTHE <1by23cs111@bmsit.in>
2026-02-24 19:30:25 +0530

    feat: enhance student fetching logic with join on student_academics table

--- Eligibility Service/utils/studentfectch.js
+++ Eligibility Service/utils/studentfectch.js
@@ -3,14 +3,15 @@ import { pool } from "../db/db.js";
 export const fetchstudent = async (criteria) => {
     try {
         const queryText = `
-            SELECT * FROM students
-            WHERE batch_year = $1
-              AND tenth_percent >= $2
-              AND twelfth_percent >= $3
-              AND ug_cgpa >= $4
-              AND branch = ANY($5)
+            SELECT s.*, sa.*
+            FROM students s
+            JOIN student_academics sa ON s.student_id = sa.student_id
+            WHERE s.graduation_year = $1
+              AND sa.tenth_percent >= $2
+              AND sa.twelfth_percent >= $3
+              AND sa.ug_cgpa >= $4
+              AND s.branch = ANY($5)
         `;
-        // Use array for allowedBranches and pass as Postgres array
         return await pool.query(queryText, [
             criteria.eligibleBatchYear,
             criteria.tenthPercent,
