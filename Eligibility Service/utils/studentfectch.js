61f89ce27cb86dbf901994b0bab6ec46a971ead4
LEELAVENKATASAISUMANTHE <1by23cs111@bmsit.in>
2026-02-24 19:45:17 +0530

    fix: refine student fetching query to select specific fields and improve readability

--- Eligibility Service/utils/studentfectch.js
+++ Eligibility Service/utils/studentfectch.js
@@ -3,7 +3,7 @@ import { pool } from "../db/db.js";
 export const fetchstudent = async (criteria) => {
     try {
         const queryText = `
-            SELECT s.*, sa.*
+            SELECT s.student_id, s.first_name, s.middle_name, s.last_name, s.full_name, s.gender, s.dob, s.email, s.alt_email, s.college_email, s.mobile
             FROM students s
             JOIN student_academics sa ON s.student_id = sa.student_id
             WHERE s.graduation_year = $1
@@ -12,13 +12,14 @@ export const fetchstudent = async (criteria) => {
               AND sa.ug_cgpa >= $4
               AND s.branch = ANY($5)
         `;
-        return await pool.query(queryText, [
+        const result = await pool.query(queryText, [
             criteria.eligibleBatchYear,
             criteria.tenthPercent,
             criteria.twelfthPercent,
             criteria.ugCgpa,
             criteria.allowedBranches
         ]);
+        return result.rows;
     } catch (error) {
         console.error("Error fetching student:", error);
         throw error;
